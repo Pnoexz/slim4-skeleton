bc711057c8e69baa1f83b4fe20531f733f258344
Matias Pino <pnoexz@gmail.com>
2020-04-11 13:05:24 -0300

    Add basic test and phpunit

---
+++ tests/Controllers/Status/HealthCheckTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests;
+
+use App\Controllers\Status\HealthCheck;
+use PHPUnit\Framework\TestCase;
+use Slim\Psr7\Response;
+
+final class HealthCheckTest extends TestCase
+{
+    public function testInvoking()
+    {
+        $controller = new HealthCheck();
+        $response = $controller(
+            $this->createStub(\Slim\Psr7\Request::class),
+            new Response()
+        );
+
+        $this->assertSame(200, $response->getStatusCode());
+    }
+}
