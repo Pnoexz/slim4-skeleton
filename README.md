71645d52984fd95ca49444da236dd6eb18616506
Matias Pino <pnoexz@gmail.com>
2020-11-18 12:51:22 -0300

    Fix namespace ambiguation in routes

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # slim4-skeleton
 
 ```bash
-composer create-project pnoexz/slim4-skeleton -s dev my-api;
+composer create-project pnoexz/slim4-skeleton -s dev --remove-vcs my-api;
 cd my-api;
-docker-compose up
+docker-compose up;
 ```
 
 Visit http://localhost/api/v0/status
--- src/routes.php
+++ src/routes.php
@@ -3,10 +3,9 @@
 namespace App;
 
 use App\Controllers\Status\HealthCheck;
-use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
-/** @var App $app */
+/** @var \Slim\App $app */
 $app->addRoutingMiddleware();
 $app->group('/api/v0', function (RouteCollectorProxy $group): void {
     $group->group('/status', function (RouteCollectorProxy $group): void {
